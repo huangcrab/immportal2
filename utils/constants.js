ac68d3299b0be0a6713f31d75a57b3ff7e1e6e7e
sean <huangcrab@gmail.com>
2019-05-05 04:59:35 -0400

    heroku try2

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,4 @@
 "use strict";
-exports.mongoUri = "mongodb://localhost:27017/immportal";
+exports.mongoUri = process.env.MONGO_URI;
 exports.PORT = process.env.PORT || 3001;
 exports.SECRET = process.env.SECRET || "super-secret-passphrase";
